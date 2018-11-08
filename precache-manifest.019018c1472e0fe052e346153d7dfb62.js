ca9cc2c5efcbb737afe95d50b683e3371f3de02c
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2018-11-08 10:08:05 +0800

    Updates

---
+++ precache-manifest.019018c1472e0fe052e346153d7dfb62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-room-booking-sample/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6e5761313c7750c39556",
+    "url": "/react-room-booking-sample/static/js/runtime~main.6e576131.js"
+  },
+  {
+    "revision": "edd69a710867abacc46b",
+    "url": "/react-room-booking-sample/static/js/main.edd69a71.chunk.js"
+  },
+  {
+    "revision": "7e4acf5b1dff87a7ce2e",
+    "url": "/react-room-booking-sample/static/js/1.7e4acf5b.chunk.js"
+  },
+  {
+    "revision": "edd69a710867abacc46b",
+    "url": "/react-room-booking-sample/static/css/main.f6cbf1fe.chunk.css"
+  },
+  {
+    "revision": "e0e78239d31156dc3b183bfbbc2063f1",
+    "url": "/react-room-booking-sample/index.html"
+  }
+];
