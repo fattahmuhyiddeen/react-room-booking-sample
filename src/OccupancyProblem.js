1205825c44ae938e3fc1076163164aab7ff8e212
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2018-11-11 07:48:31 +0800

    bug fix

--- src/OccupancyProblem.js
+++ src/OccupancyProblem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+const MAX_NUMBER = 999 //to indicate rejected booking
 const styles = {
   inputContainer: {
     margin: '10px'
@@ -27,19 +28,37 @@ export default class App extends Component {
 
   calculateResult = () => {
     const { adult, children, infant } = this.state
+    let room = 0
+    if (adult == 0) {
+      room = 0
+    }
+    if (adult >= children && adult >= infant) {
+      room = Math.ceil(adult / 3)
+    } else if (children > adult && children >= infant) {
+      const ratioChildrenAdult = Math.ceil(children / adult)
+      room = ratioChildrenAdult > 3 ? MAX_NUMBER : Math.ceil(children / 3)
+    } else if (infant > adult && infant >= children) {
+      const ratioInfantAdult = Math.ceil(infant / adult)
+      room = ratioInfantAdult > 3 ? MAX_NUMBER : Math.ceil(infant / 3)
+    }
+
+    this.setState({ room })
   }
+  handleFocus = (event) => event.target.select()
+
   render() {
     const { adult, children, infant, room } = this.state
     return (
       <div>
-        <div style={styles.inputContainer} >Adult : <input onChange={(e) => this.onChange('adult', e.target.value)} value={adult} /></div>
-        <div style={styles.inputContainer} >Children : <input onChange={(e) => this.onChange('children', e.target.value)} value={children} /></div>
-        <div style={styles.inputContainer} >Infant : <input onChange={(e) => this.onChange('infant', e.target.value)} value={infant} /></div>
+        <div style={styles.inputContainer} >Adult : <input onFocus={this.handleFocus} onChange={(e) => this.onChange('adult', e.target.value)} value={adult} /></div>
+        <div style={styles.inputContainer} >Children : <input onFocus={this.handleFocus} onChange={(e) => this.onChange('children', e.target.value)} value={children} /></div>
+        <div style={styles.inputContainer} >Infant : <input onFocus={this.handleFocus} onChange={(e) => this.onChange('infant', e.target.value)} value={infant} /></div>
         {room > 3 || (adult + children > 7) ?
           <div>Booking Rejected</div>
           :
           <div>Number of room{room > 1 ? 's' : ''} can be fit in : {room}</div>
         }
+        {room == MAX_NUMBER && <div>Not enough adult to look after children / infant</div>}
       </div>
     );
   }
